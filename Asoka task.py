885849ff856ee7dd5de5a9a8fb18aa47e38ecae0
unknown <Salya.wirachman@gmail.com>
2018-09-19 11:46:21 +0700

    Firts commit

--- Asoka task.py
+++ Asoka task.py
@@ -9,9 +9,62 @@ for i in range(1,number+1):
     print(" " * (number-i),"*" * i)
 
 number = int(input("Input Number:"))
+
 for i in range(1,number+1):
     print("*" * (number-i+1))
 
 number = int(input("Input Number:"))
-for i in range(1,number+1):
-    print(" " * (number+1),"*" * (number-1+i))
+
+for i in range(0,number+1):
+    print(" "* i,"*"*number)
+    number = number-1
+#IDK the same side triangles so i googled it
+number = int(input("Input Number:"))
+
+for i in range(0,number):
+    for o in range(0,number-i):
+        print(end=" ")
+        print(end=" ")
+    for o in range (0,i+1):
+        print("*",end=" ")
+    for o in range (0,i):
+        print ("*",end=" ")
+    print("")
+print(" ")
+
+number = int(input("Input Number:"))
+
+for i in range(0,number):
+    for o in range(0, i+1):
+        print(end=" ")
+        print(end=" ")
+    for o in range(0,number-i):
+        print(" ",end="*")
+    for o in range(0, number - i-1):
+        print(" ",end="*")
+    print("")
+print("")
+
+number = int(input("Input Number:"))
+
+for i in range(0, number):
+    for o in range(0, number - i):
+        print(end=" ")
+        print(end=" ")
+    for o in range(0, i + 1):
+        print("*", end=" ")
+    for o in range(0, i):
+        print("*", end=" ")
+    print("")
+for i in range(0,number):
+    for o in range(1):
+        print(" ",end="")
+    for o in range(0, i+1):
+        print(end=" ")
+        print(end=" ")
+    for o in range(0,number-i-1):
+        print(" ",end="*")
+    for o in range(0, number - i-2):
+        print(" ",end="*")
+    print("")
+print("")
